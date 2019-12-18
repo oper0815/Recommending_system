3f0be23307f1576eaa65cf017f297ca2f13b6076
oper0815 <37662783+oper0815@users.noreply.github.com>
2019-12-18 11:24:20 +0900

    Update colab_recommending.py

--- study/colab_recommending.py
+++ study/colab_recommending.py
@@ -1,1 +1,2 @@
-test
+# link
+https://colab.research.google.com/github/google/eng-edu/blob/master/ml/recommendation-systems/recommendation-systems.ipynb?utm_source=ss-recommendation-systems&utm_campaign=colab-external&utm_medium=referral&utm_content=recommendation-systems#scrollTo=KhAVFaqCo4s5
