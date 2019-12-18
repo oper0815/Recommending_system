5066f2d85464f42eb8b5e6d07ba84df13a944df6
oper0815 <37662783+oper0815@users.noreply.github.com>
2019-12-18 13:20:26 +0900

    Update colab_recommending.py

--- study/colab_recommending.py
+++ study/colab_recommending.py
@@ -1,2 +1,45 @@
 # link
 # https://colab.research.google.com/github/google/eng-edu/blob/master/ml/recommendation-systems/recommendation-systems.ipynb?utm_source=ss-recommendation-systems&utm_campaign=colab-external&utm_medium=referral&utm_content=recommendation-systems#scrollTo=KhAVFaqCo4s5
+# colab
+
+from __future__ import print_function
+
+import numpy as np
+import pandas as pd
+import collections
+from mpl_toolkits.mplot3d import Axes3D
+from IPython import display
+from matplotlib import pyplot as plt
+import sklearn
+import sklearn.manifold
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+# Add some convenience functions to Pandas DataFrame.
+pd.options.display.max_rows = 10
+pd.options.display.float_format = '{:.3f}'.format
+def mask(df, key, function):
+  """Returns a filtered dataframe, by applying function to key"""
+  return df[function(df[key])]
+
+def flatten_cols(df):
+  df.columns = [' '.join(col).strip() for col in df.columns.values]
+  return df
+
+pd.DataFrame.mask = mask
+pd.DataFrame.flatten_cols = flatten_cols
+
+# Install Altair and activate its colab renderer.
+print("Installing Altair...")
+!pip install git+git://github.com/altair-viz/altair.git
+import altair as alt
+alt.data_transformers.enable('default', max_rows=None)
+alt.renderers.enable('colab')
+print("Done installing Altair.")
+
+# Install spreadsheets and import authentication module.
+USER_RATINGS = False
+!pip install --upgrade -q gspread
+from google.colab import auth
+import gspread
+from oauth2client.client import GoogleCredentials
